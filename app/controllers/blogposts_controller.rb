c497e5fd4e5d96bbdb389bc60312168c59b6155e
Daksh <dakshces@gmail.com>
2021-04-22 12:17:38 +0000

    Add posts new, edit, index

--- app/controllers/blogposts_controller.rb
+++ app/controllers/blogposts_controller.rb
@@ -1,2 +1,47 @@
 class BlogpostsController < ApplicationController
+  
+  def index
+    @blogposts = Blogpost.all
+  end
+  
+  def new 
+    @blogpost = Blogpost.new
+  end
+  
+  def create
+    #render plain: params[:blogpost].inspect
+    @blogpost = Blogpost.new(blogpost_params)
+    if @blogpost.save
+      flash[:notice] = "Blogpost was successfully created"
+      redirect_to blogpost_path(@blogpost)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @blogpost = Blogpost.find(params[:id])
+  end
+  
+  def edit
+    @blogpost = Blogpost.find(params[:id])
+  end
+  
+  def update
+    @blogpost = Blogpost.find(params[:id])
+    if @blogpost.update(blogpost_params)
+     flash[:notice] = "Blogpost was updated"
+     redirect_to blogpost_path(@blogpost)
+    else
+     flash[:notice] = "Blogpost was not updated"
+     render 'edit'
+    end
+  end
+  
+  private
+  def blogpost_params
+   params.require(:blogpost).permit(:title, :content, :tags)
+  end
+
+
 end
--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -1,3 +1,5 @@
 class Blogpost < ApplicationRecord
   serialize :tags,Array
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :content, presence: true
 end
