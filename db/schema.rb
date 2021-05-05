0a8b2c344692568fbe08d54764b7269a75d64feb
Daksh <dakshces@gmail.com>
2021-05-05 09:35:22 +0000

    Add user signup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_03_125719) do
+ActiveRecord::Schema.define(version: 2021_05_05_091817) do
 
   create_table "blogposts", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2021_05_03_125719) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
